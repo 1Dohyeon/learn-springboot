ea64fd84eb8eeaecd55155447b220651934cad47
1Dohyeon <dh1072005@gmail.com>
2023-07-04 13:43:50 +0900

    config 수정

--- practice01/demo/src/main/java/practice1/spring/demo/AppConfig.java
+++ practice01/demo/src/main/java/practice1/spring/demo/AppConfig.java
@@ -1,6 +1,8 @@
 package practice1.spring.demo;
 
+import practice1.spring.demo.Discount.DiscountPolicy;
 import practice1.spring.demo.Discount.FixDiscountPolicy;
+import practice1.spring.demo.Member.MemberRepository;
 import practice1.spring.demo.Member.MemberService;
 import practice1.spring.demo.Member.MemberServiceImpl;
 import practice1.spring.demo.Member.MemoryMemberRepository;
@@ -9,13 +11,20 @@ import practice1.spring.demo.Order.OrderServiceImpl;
 
 public class AppConfig {
 
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy(); // <- 할인 정책 변경시에 이 부분만 수정하면 됨
+    }
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
 }
