c1629714ac932bc6a29ca987b4136da6c8d6ae29
1Dohyeon <dh1072005@gmail.com>
2023-07-13 20:23:49 +0900

    스프링빈 적용

--- PRACTICE_01_01/demo/src/main/java/spring/practice01/demo/AppConfig.java
+++ PRACTICE_01_01/demo/src/main/java/spring/practice01/demo/AppConfig.java
@@ -1,5 +1,8 @@
 package spring.practice01.demo;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import spring.practice01.demo.card.CardMemory;
 import spring.practice01.demo.card.CardMemoryImpl;
 import spring.practice01.demo.card.CardService;
@@ -7,17 +10,21 @@ import spring.practice01.demo.card.CardServiceImpl;
 import spring.practice01.demo.member.MemoryMember;
 import spring.practice01.demo.member.MemoryMemberImpl;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public CardService cardService() {
         return new CardServiceImpl(
                 new CardMemoryImpl(),
                 new MemoryMemberImpl());
     }
 
+    @Bean
     public MemoryMember memoryMember() {
         return new MemoryMemberImpl();
     }
 
+    @Bean
     public CardMemory cardMemory() {
         return new CardMemoryImpl();
     }
--- PRACTICE_01_01/demo/src/main/java/spring/practice01/demo/CardApp.java
+++ PRACTICE_01_01/demo/src/main/java/spring/practice01/demo/CardApp.java
@@ -1,5 +1,9 @@
 package spring.practice01.demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
 import spring.practice01.demo.card.Card;
 import spring.practice01.demo.card.CardService;
 import spring.practice01.demo.member.Member;
@@ -7,13 +11,14 @@ import spring.practice01.demo.member.MemoryMember;
 
 public class CardApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemoryMember memoryMember = appConfig.memoryMember();
-        CardService cardService = appConfig.cardService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemoryMember memoryMember = applicationContext.getBean("memoryMember", MemoryMember.class);
+        CardService cardService = applicationContext.getBean("cardService", CardService.class);
+        ((AbstractApplicationContext) applicationContext).close();
 
         Member member1 = new Member("abc123", "qwer1234", "Kim");
         Member member2 = new Member("xyz123", "qwer1234", "Park");
-        Member member3 = new Member("xyz123", "abcd1234", "Lee");
+        Member member3 = new Member("kim123", "abcd1234", "Kim");
         System.out.println("---------------------------------");
 
         memoryMember.save(member1);
@@ -23,17 +28,26 @@ public class CardApp {
 
         memoryMember.findById("abc123");
         memoryMember.findById("xyz123");
-        memoryMember.findById("qwer1234");
+        memoryMember.findById("kim123");
         System.out.println("---------------------------------");
 
         Card card1 = cardService.createCard(member1, "KIMCARD", 0);
+        System.out.println("---------------------------------");
         Card card2 = cardService.createCard(member2, "PARKCARD", 5000);
-        Card card3 = cardService.createCard(member3, "LEECARD", 10000);
+        System.out.println("---------------------------------");
+        Card card3 = cardService.createCard(member2, "PARKCARD", 5000); // 똑같은 사람이 카드를 더 만듦
+        System.out.println("---------------------------------");
+        Card card4 = cardService.createCard(member3, "KIMCARD", 5000); // 같은 이름의 카드가 존재함
         System.out.println("---------------------------------");
 
         System.out.println(card1.getCardName() + " = " + card1.getPoint());
         System.out.println(card2.getCardName() + " = " + card2.getPoint());
-        System.out.println(card3.getCardName() + " = " + card3.getPoint());
+        System.out.println("---------------------------------");
 
+        System.out.println("abc123님의 카드 이름은 " + cardService.findCardNameById("abc123"));
+        System.out.println("---------------------------------");
+        System.out.println("xyz123님의 카드 이름은 " + cardService.findCardNameById("xyz123"));
+        System.out.println("---------------------------------");
+        System.out.println("qwer1234님의 카드 이름은 " + cardService.findCardNameById("kim123"));
     }
 }
