19f1626244402459253950c0530c62c71e058a1d
1Dohyeon <dh1072005@gmail.com>
2023-07-24 15:49:04 +0900

    수정

--- PRACTICE_01_01/demo/src/main/java/spring/practice01/demo/AutoAppConfig.java
+++ PRACTICE_01_01/demo/src/main/java/spring/practice01/demo/AutoAppConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.FilterType;
 // 컴포넌트 스캔을 사용하려면 먼저 @ComponentScan 을 설정 정보에 붙여주면 됨.
 // @Bean 등록을 안해도됨.
 @ComponentScan(excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
-
 public class AutoAppConfig {
 
 }
